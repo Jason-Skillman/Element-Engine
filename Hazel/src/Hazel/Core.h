fa1d455f2cdd49d8bc3bac2331256f1de81a5448
JasonSkillman <jason_skillman@outlook.com>
2021-01-22 10:56:36 -0500

    Move assert defines into core.h

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,5 +1,6 @@
 #pragma once
 
+//Windows DLL
 #ifdef HZ_PLATFORM_WINDOWS
 	#ifdef HZ_BUILD_DLL
 		#define HAZEL_API __declspec(dllexport)
@@ -9,6 +10,21 @@
 #else
 	#error Hazel Engine only supports Windows
 #endif
+
+
+//Asserts
+#ifdef HZ_DEBUG
+	#define HZ_ENABLE_ASSERTS 1
+#endif
+
+#if HZ_ENABLE_ASSERTS
+	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define HZ_ASSERT(x, ...)
+	#define HZ_CORE_ASSERT(x, ...)
+#endif
+
 
 #define BIT(x) (1 << x)
 
