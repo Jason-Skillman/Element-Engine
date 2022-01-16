3d1a728fba54ca18ceea68fca83bed2acfd2778a
Jason Skillman <jason_skillman@outlook.com>
2022-01-16 12:49:41 -0500

    Add file icons to content browser

--- ElementEditor/src/Panels/ContentBrowserPanel.cpp
+++ ElementEditor/src/Panels/ContentBrowserPanel.cpp
@@ -8,10 +8,14 @@
 namespace Element {
 
 	//Todo: Change directory when project locations are added
-	static const std::filesystem::path assetsPath = "assets";
+	static const std::filesystem::path assetsPath = "Assets";
 
 	ContentBrowserPanel::ContentBrowserPanel()
-		: currentDirectory(assetsPath) {}
+		: currentDirectory(assetsPath) {
+	
+		directoryIcon = Texture2D::Create("Resources/Icons/directory_icon.png");
+		fileIcon = Texture2D::Create("Resources/Icons/file_icon.png");
+	}
 
 	void ContentBrowserPanel::OnImGuiRender() {
 		ImGui::Begin("Content Browser");
@@ -27,33 +31,54 @@ namespace Element {
 			}
 		}
 
-		//Iterate through all files and display them
-		for(auto& dirEntry : std::filesystem::directory_iterator(currentDirectory)) {
-			//Path of current element
-			const auto& path = dirEntry.path();
-			std::string pathStr = path.string();
+		static float padding = 16.0f;
+		static float thumbnailSize = 128.0f;
+		float cellSize = thumbnailSize + padding;
 
-			auto filename = path.filename();
-			auto filenameStr = filename.string();
+		float panelWidth = ImGui::GetContentRegionAvail().x;
+		int columnCount = (int)(panelWidth / cellSize);
+		if(columnCount < 1)
+			columnCount = 1;
 
-			//Alternateive to above filename. Displays parent folder relative to current path.
-			//auto relativePath = std::filesystem::relative(path, assetsPath);
-			//std::string relativePathStr = relativePath.string();
-			
-			//Check if path is a directory
-			if(dirEntry.is_directory()) {
-				//Display folder
-				if(ImGui::Button(filenameStr.c_str())) {
-					//Navigate inside the folder
-					
-					currentDirectory /= filename;
+		{
+			ImGui::Columns(columnCount, 0, false);
+
+			//Iterate through all files and display them
+			for(auto& dirEntry : std::filesystem::directory_iterator(currentDirectory)) {
+				//Path of current element
+				const auto& path = dirEntry.path();
+				std::string pathStr = path.string();
+
+				auto filename = path.filename();
+				std::string filenameStr = filename.string();
+
+				//Alternateive to above filename. Displays parent folder relative to current path.
+				//auto relativePath = std::filesystem::relative(path, assetsPath);
+				//std::string relativePathStr = relativePath.string();
+
+				//Switch image depending on directory or file
+				Ref<Texture2D> icon = dirEntry.is_directory() ? directoryIcon : fileIcon;
+				ImGui::ImageButton((ImTextureID)icon->GetRendererID(), { thumbnailSize, thumbnailSize }, { 0, 1 }, { 1, 0 });
+
+				//Button click
+				if(ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
+					//Check if path is a directory
+					if(dirEntry.is_directory()) {
+						//Navigate inside the folder
+						currentDirectory /= filename;
+					}
 				}
-			} else {
-				//Display file
-				if(ImGui::Button(filenameStr.c_str())) {
-					
-				}
+
+				//Display the filename
+				ImGui::TextWrapped(filenameStr.c_str());
+
+				ImGui::NextColumn();
 			}
+
+			ImGui::Columns(1);
+
+			//ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 512);
+			//ImGui::SliderFloat("Padding", &padding, 0, 32);
 		}
 
 		ImGui::End();
--- ElementEditor/src/Panels/ContentBrowserPanel.h
+++ ElementEditor/src/Panels/ContentBrowserPanel.h
@@ -2,11 +2,15 @@
 
 #include <filesystem>
 
+#include "Element/Renderer/Texture.h"
+
 namespace Element {
 
 	class ContentBrowserPanel {
 	private:
 		std::filesystem::path currentDirectory;
+
+		Ref<Texture2D> directoryIcon, fileIcon;
 
 	public:
 		ContentBrowserPanel();
